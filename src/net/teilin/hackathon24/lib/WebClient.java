902c96a9a4ab5639406913488ec563114f747d96
zaker <zaker@THOX>
2011-10-30 15:25:20 +0100

    Refractored webclient

--- src/net/teilin/hackathon24/lib/WebClient.java
+++ src/net/teilin/hackathon24/lib/WebClient.java
@@ -2,7 +2,6 @@ package net.teilin.hackathon24.lib;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -59,8 +58,10 @@ public class WebClient {
 		myCookies.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
 		try {
 
-			//keyValue.add(new BasicNameValuePair("foedselsnummer", user.getPID()));
-			//keyValue.add(new BasicNameValuePair("passord", user.getPassword()));
+			// keyValue.add(new BasicNameValuePair("foedselsnummer",
+			// user.getPID()));
+			// keyValue.add(new BasicNameValuePair("passord",
+			// user.getPassword()));
 			keyValue.add(new BasicNameValuePair("foedselsnummer", "30088847993"));
 			keyValue.add(new BasicNameValuePair("passord", "oipt737OI!"));
 			post.setEntity(new UrlEncodedFormEntity(keyValue));
@@ -86,100 +87,108 @@ public class WebClient {
 		}
 		return false;
 	}
-	
+
 	public void Write2File(InputStream is) throws Exception {
 		File root = Environment.getExternalStorageDirectory();
-		FileOutputStream f = new FileOutputStream(new File(root,"test.pdf"));
+		FileOutputStream f = new FileOutputStream(new File(root, "test.pdf"));
 		byte[] buffer = new byte[1024];
 		int len1 = 0;
-		while((len1 = is.read(buffer))>0) {
-			f.write(buffer,0,len1);
+		while ((len1 = is.read(buffer)) > 0) {
+			f.write(buffer, 0, len1);
 		}
 		f.close();
 	}
-	
+
 	public void speakFile() throws IOException {
 		File root = Environment.getExternalStorageDirectory();
 		try {
-		Pdf2Text p2t = new Pdf2Text(root.toString()+"/test.pdf");
-		String text = p2t.getText();
-		}catch(Exception e){
-			Log.e(LOG_TAG,root.toString()+"/test.pdf");
+			Pdf2Text p2t = new Pdf2Text(root.toString() + "/test.pdf");
+			String text = p2t.getText();
+		} catch (Exception e) {
+			Log.e(LOG_TAG, root.toString() + "/test.pdf");
 		}
-		Log.v("TEXT",root.toString()+"/test.pdf");
+		Log.v("TEXT", root.toString() + "/test.pdf");
 	}
-	
-	public List<String> getKitchenContent() { 
-		BufferedReader in = null;
-		List<String> stringList = new ArrayList<String>();
-		try{
-			Log.v(LOG_TAG, "Getting account");
-			HttpGet get = new HttpGet(
-					"https://www.digipost.no/post/privat/konto");
+
+	public String getStringFromURI(String uri) {
+		HttpGet get = new HttpGet(uri);
+		try {
 			HttpResponse response = httpClient.execute(get);
-			Log.v(LOG_TAG, response.getStatusLine().toString());
-			
-			in = new BufferedReader
-		            (new InputStreamReader(response.getEntity().getContent()));
-			
-		    StringBuffer sb = new StringBuffer("");
-		    String line = "";
-		    String NL = System.getProperty("line.separator");
-		    while ((line = in.readLine()) != null) {
-		    	sb.append(line + NL);
-		    }
-		    
-		    in.close();
-		    String jString = sb.toString();
-		    
-		    JSONObject jObject = new JSONObject(jString);
-		    
-		    String kitchenURI = jObject.getString("kjokkenbenkUri");
-		    Log.v("kjokkenbenkUri",kitchenURI);
-		    get = new HttpGet(kitchenURI);
-		    
-		    response = httpClient.execute(get);
-		    Log.v(LOG_TAG, response.getStatusLine().toString());
-			
-			in = new BufferedReader
-		            (new InputStreamReader(response.getEntity().getContent()));
-			
-		    sb = new StringBuffer("");
-		    line = "";
-		    
-		    while ((line = in.readLine()) != null) {
-		    	sb.append(line + NL);
-		    }
-		    
-		    in.close();
-		    jString = sb.toString();
-		    
-		    JSONArray jArray = new JSONArray(jString);
-		    JSONObject firstLetter = jArray.getJSONObject(0);
-		    String letterURI = firstLetter.getString("brevUri");
-		    get = new HttpGet(letterURI);
-		    response = httpClient.execute(get);
-		    
-		    InputStream inS = response.getEntity().getContent();
-		    Write2File(inS);
-		    Log.v("Stream","Stream");
-		    speakFile();
-			
-		    
-	    } catch (Exception e) {
-			Log.e(LOG_TAG,e.getMessage());
-	    	if (in != null) {
-	    		try {
-	    			in.close();
-	    		} catch (IOException ef) {
-	    			Log.e(LOG_TAG,ef.getMessage());
-	    		}
-	    	}
-		
-		
-		
-	    }
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+
+			in.close();
+			String jString = sb.toString();
+
+			return jString;
+		} catch (Exception e) {
+			Log.e(LOG_TAG, e.getMessage());
+			return null;
+		}
+	}
+
+	public JSONObject getJSONFromURI(String uri) {
+
+		String jString = getStringFromURI(uri);
+		try {
+			JSONObject jObject = new JSONObject(jString);
+
+			return jObject;
+		} catch (Exception e) {
+			Log.e(LOG_TAG, e.getMessage());
+			return null;
+		}
+
+	}
+
+	private JSONArray getJSONArrayFromURI(String uri) {
+		String jString = getStringFromURI(uri);
+		try {
+			JSONArray jArray = new JSONArray(jString);
+
+			return jArray;
+		} catch (Exception e) {
+			Log.e(LOG_TAG, e.getMessage());
+			return null;
+		}
+	}
+
+	public List<String> getKitchenContent() {
+
+		List<String> stringList = new ArrayList<String>();
+		try {
+			Log.v(LOG_TAG, "Getting account");
+			JSONObject jObject = getJSONFromURI("https://www.digipost.no/post/privat/konto");
+			String kitchenURI = jObject.getString("kjokkenbenkUri");
+			Log.v("kjokkenbenkUri", kitchenURI);
+			JSONArray jArray = getJSONArrayFromURI(kitchenURI);
+
+			JSONObject firstLetter = jArray.getJSONObject(0);
+
+			String letterURI = firstLetter.getString("brevUri");
+
+			HttpGet get = new HttpGet(letterURI);
+			response = httpClient.execute(get);
+
+			InputStream inS = response.getEntity().getContent();
+			Write2File(inS);
+			Log.v("Stream", "Stream");
+			speakFile();
+
+		} catch (Exception e) {
+			Log.e(LOG_TAG, e.getMessage());
+
+		}
 		return stringList;
 	}
-//		return stringList;
+	// return stringList;
+
 }
